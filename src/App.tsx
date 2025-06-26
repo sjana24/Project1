beff323570fa518a16e2b16ca94b715e6363d367
sjana24 <141610669+sjana24@users.noreply.github.com>
2025-06-26 11:23:55 +0530

    create login page for customer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
+import LoginCustomer from "./pages/LoginCustomer";
 const App = () => (
 
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Index />} />
+      <Route path="/login" element={<LoginCustomer/>} />
     </Routes>
 
   </BrowserRouter>
---
+++ src/pages/LoginCustomer.tsx
@@ -0,0 +1,13 @@
+
+
+
+const Login=()=>{
+
+    return(
+        <>
+            <h1>login</h1>
+        </>
+    );
+
+};
+export default Login;
