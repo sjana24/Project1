7723b633db11d7e6054277c4f4ec85a7b68d45d1
sjana24 <141610669+sjana24@users.noreply.github.com>
2025-07-02 22:22:48 +0530

    update get All services and products

--- Backend/GetAllServicesCustomer.php
+++ Backend/GetAllServicesCustomer.php
@@ -1,14 +1,16 @@
- <?php
+<?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-require_once "./Root/Product.php";
+
+echo "hi";
+require_once "./Root/Service.php";
 
 
 
 
-$getAll=new Product();
-$response=$getAll->getAllProducts();
+$getAll=new Service();
+$response=$getAll->getAllServices();
 
 echo json_encode($response);
