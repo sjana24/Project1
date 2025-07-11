c2632596cddfe159684f9a9bb6403d739be30cba
sjana24 <141610669+sjana24@users.noreply.github.com>
2025-07-11 20:27:30 +0530

    admin product page basic setup

---
+++ Backend/GetAllProductAdmin.php
@@ -0,0 +1,14 @@
+ <?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+require_once "./Root/Product.php";
+
+
+
+
+$getAll=new Product();
+$response=$getAll->getAllProductsAdmin();
+
+echo json_encode($response);
