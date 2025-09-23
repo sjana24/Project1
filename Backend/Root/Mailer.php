5f69c3caedce8302237ca0cea61e96016cf40379
Makinthan2001 <mahinthan2001a@gmail.com>
2025-09-23 16:40:52 +0530

    update SMTP credentials and add OTP JSON files

--- Backend/Root/Mailer.php
+++ Backend/Root/Mailer.php
@@ -20,12 +20,12 @@ class Mailer
         $this->mail->isSMTP();
         $this->mail->Host       = 'smtp.gmail.com';
         $this->mail->SMTPAuth   = true;
-        $this->mail->Username   = 'madhan2001ana@gmail.com'; // SMTP email
-        $this->mail->Password   = 'ilergdrkkdycocoh'; // SMTP password
+        $this->mail->Username   = 'srijanakan24@gmail.com'; // SMTP email
+        $this->mail->Password   = 'gtmzquwzcgtbadpt'; // SMTP password
         $this->mail->SMTPSecure = 'ssl';
         $this->mail->Port       = 465;
         //mahinthan2001a@gmail.com  nmmivtjmwmufbszb
-        $this->mail->setFrom('madhan2001ana@gmail.com', 'GearSphere');
+        $this->mail->setFrom('srijankan24@gmail.com', 'SolaX');
     }
 
     // Enhanced method with neat templates
